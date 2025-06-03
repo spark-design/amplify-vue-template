baf281e509479737abdc891eb4b17b99213b7f10
Sparkman <sparkcon@amazon.com>
2025-06-03 12:49:12 -0400

    Adding lambda function

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,9 +2,11 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
+import { myFirstFunction } from './my-first-function/resource';
 
 defineBackend({
   auth,
   data,
-  storage
+  storage,
+  myFirstFunction
 });
